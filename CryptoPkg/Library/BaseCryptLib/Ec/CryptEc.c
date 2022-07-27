20577c20eb5f87017695a21e4b0b4fe467c9e4d9
Yi Li <yi1.li@intel.com>
2022-07-27 17:19:27 +0800

    add a trick to gen key
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- CryptoPkg/Library/BaseCryptLib/Ec/CryptEc.c
+++ CryptoPkg/Library/BaseCryptLib/Ec/CryptEc.c
@@ -522,16 +522,17 @@ EcDhGenerateKey (
   EcKey = (EC_KEY *)EcContext;
   Group = EC_KEY_get0_group (EcKey);
   HalfSize = (EC_GROUP_get_degree (Group) + 7) / 8;
-  if (*PublicKeySize != HalfSize * 2) {
-    *PublicKeySize = HalfSize * 2;
-    return FALSE;
-  }
-  *PublicKeySize = HalfSize * 2;
 
   // Assume RAND_seed was called
   if (EC_KEY_generate_key (EcKey) != 1) {
     return FALSE;
   }
+
+  if (*PublicKeySize != HalfSize * 2) {
+    *PublicKeySize = HalfSize * 2;
+    return FALSE;
+  }
+  *PublicKeySize = HalfSize * 2;
 
   EcPoint = EC_KEY_get0_public_key (EcKey);
   if (EcPoint == NULL) {
