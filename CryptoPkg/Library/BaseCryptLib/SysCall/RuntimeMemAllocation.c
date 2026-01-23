88cfacbf10e00429abf30a2602b9fbc100526d71
Yi Li <yi1.li@intel.com>
2026-01-23 12:25:18 +0800

    fix: enlarge mem for ml-dsa keys
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- CryptoPkg/Library/BaseCryptLib/SysCall/RuntimeMemAllocation.c
+++ CryptoPkg/Library/BaseCryptLib/SysCall/RuntimeMemAllocation.c
@@ -33,7 +33,12 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define RT_PAGE_FREE  0x00000000
 #define RT_PAGE_USED  0x00000001
 
-#define MIN_REQUIRED_BLOCKS  1100
+//
+// Increase memory pool size to reduce fragmentation for ML-DSA operations
+// Original: 1100 blocks (1.1 MB)
+// New: 2048 blocks (2 MB) - provides more headroom for large keys
+//
+#define MIN_REQUIRED_BLOCKS  2048
 
 //
 // Memory Page Table
